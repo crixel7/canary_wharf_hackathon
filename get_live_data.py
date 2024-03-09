75bff1144af2ef9fca494e660fd2bba908efc19f
crixel7 <cristianiancu.personal@gmail.com>
2024-03-09 09:39:00 +0000

    Removed my API key from the public code

--- get_live_data.py
+++ get_live_data.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 def fetch_summoner_id(summoner_name, api_key):
@@ -5,17 +6,19 @@ def fetch_summoner_id(summoner_name, api_key):
     summoner_data = response.json()
     return summoner_data['id']
 
-# Fetch summoner ID for a summoner
-summoner_id = fetch_summoner_id('eden', 'RGAPI-66961300-3b33-434a-9718-505ac10c7b07')
-
 def fetch_live_game_data(summoner_id, api_key):
     response = requests.get(f'https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summoner_id}?api_key={api_key}')
     game_data = response.json()
     return game_data
 
-# Fetch live game data for a summoner
-game_data = fetch_live_game_data(summoner_id, 'RGAPI-66961300-3b33-434a-9718-505ac10c7b07')
+# Get the API key from an environment variable
+api_key = os.getenv('RIOT_API_KEY')
+
+# Fetch the encrypted summoner ID
+summoner_id = fetch_summoner_id('eden', api_key)
+
+# Fetch live game data
+game_data = fetch_live_game_data(summoner_id, api_key)
 
 print(summoner_id)
-
 print(game_data)
